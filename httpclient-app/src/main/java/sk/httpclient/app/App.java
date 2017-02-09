57934062cdd693f16bfb41acbbc2078a250ad19c
Ľubomír Varga <lubomir.varga@nike.sk>
2017-02-09 14:16:44 +0100

    Added also SynchronizedSummaryStatistics to calculate "utilization".
    Also changed a bit reporting code and make some constants.
    Sample report output:
    ```
    terminated OK=true
    Statistics=SummaryStatistics:
    n: 492
    min: 0.900914
    max: 3329.875544
    sum: 54534.46580400002
    mean: 110.84241017073163
    geometric mean: 61.43471089836358
    variance: 75858.29253065416
    population variance: 75704.10900925039
    second moment: 3.724642163255119E7
    sum of squares: 4.329115325963989E7
    standard deviation: 275.4238416162518
    sum of logs: 2026.0436997516492
    
    2/9/17 2:23:45 PM ==============================================================
    
    -- Timers ----------------------------------------------------------------------
    http-timer
                 count = 492
             mean rate = 33.65 calls/second
         1-minute rate = 7.97 calls/second
         5-minute rate = 5.14 calls/second
        15-minute rate = 4.65 calls/second
                   min = 0.88 milliseconds
                   max = 3329.84 milliseconds
                  mean = 107.26 milliseconds
                stddev = 264.30 milliseconds
                median = 61.05 milliseconds
                  75% <= 93.40 milliseconds
                  95% <= 273.97 milliseconds
                  98% <= 621.68 milliseconds
                  99% <= 1065.83 milliseconds
                99.9% <= 3329.84 milliseconds
    
    Total duration (wall clock) = 15672ms. "utilization"=3.479738757274121
    ```

--- httpclient-app/src/main/java/sk/httpclient/app/App.java
+++ httpclient-app/src/main/java/sk/httpclient/app/App.java
@@ -1,78 +1,69 @@
 package sk.httpclient.app;
 
-
 import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
 import com.netflix.client.ClientException;
+import org.apache.commons.math3.stat.descriptive.SynchronizedSummaryStatistics;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.concurrent.*;
 
 public class App {
+    private static final boolean DOSLEEP = false;
+    private static final int REQUESTSINSINGLERUN = 123;
+    private static final int NUMBEROFGENERATEDTASKS = 4;
+
     public static void main(String[] args) throws InterruptedException, IOException, ClientException, URISyntaxException, ExecutionException {
+        long startTotal = System.currentTimeMillis();
         RibbonHttpClient<Record, Record> r = new RibbonHttpClient<>("http://localhost:8888, http://localhost:8889, http://localhost:8887");
+        SynchronizedSummaryStatistics s = new SynchronizedSummaryStatistics();
         MetricRegistry metricRegistry = new MetricRegistry();
         Timer timer = metricRegistry.timer("http-timer");
         ConsoleReporter reporter = ConsoleReporter.forRegistry(metricRegistry)
                 .convertRatesTo(TimeUnit.SECONDS)
                 .convertDurationsTo(TimeUnit.MILLISECONDS)
                 .build();
-        reporter.start(300, TimeUnit.MILLISECONDS);
+        reporter.start(10, TimeUnit.SECONDS);
 
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    runRequests(r, timer);
-                } catch (ExecutionException | InterruptedException e) {
-                    e.printStackTrace();
-                }
+        Runnable runnable = () -> {
+            try {
+                runRequests(r, timer, s);
+            } catch (ExecutionException | InterruptedException e) {
+                System.out.println("Error in requests. ex=" + e.getMessage());
             }
         };
 
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(100);
+        ExecutorService executor = Executors.newFixedThreadPool(20);
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < NUMBEROFGENERATEDTASKS; i++) {
             executor.execute(runnable);
         }
 
-//        ArrayList<Server> servers = Lists.newArrayList(
-//                new Server("localhost", 8886),
-//                new Server("localhost", 8887),
-//                new Server("localhost", 8881));
-//
-//        ExperimentalClient<String, Record> client = new ExperimentalClient(servers);
-//
-//        for (int i = 0; i < 10; i++) {
-//            Record record = null;
-//            try {
-//                Future<Record> send = client.send("/test/test", null, Record.class);
-//                record = send.get();
-//            } catch (ExecutionException | InterruptedException e) {
-//            }
-//            System.out.println("record: " + (record == null ? "kakanica" : record.getCity()));
-//        }
-//
-
         executor.shutdown();
-        executor.awaitTermination(100, TimeUnit.DAYS);
+        System.out.println("terminated OK=" + executor.awaitTermination(100, TimeUnit.SECONDS));
+        System.out.println("Statistics=" + s);
+        reporter.report();
+        long totalDutation = System.currentTimeMillis() - startTotal;
+        System.out.println("Total duration (wall clock) = " + totalDutation + "ms. \"utilization\"=" + (s.getSum() / totalDutation) + "");
         System.exit(0);
     }
 
-    private static void runRequests(RibbonHttpClient<Record, Record> r, Timer timer) throws InterruptedException, ExecutionException {
-
-        for (int i = 0; i < 500; i++) {
+    private static void runRequests(RibbonHttpClient<Record, Record> r, Timer timer, SynchronizedSummaryStatistics s) throws InterruptedException, ExecutionException {
+        for (int i = 0; i < REQUESTSINSINGLERUN; i++) {
+            long start = System.nanoTime();
             try (Timer.Context ctx = timer.time()) {
                 //  long startTime = System.nanoTime();
                 Future<Record> aaa = r.send("/test", null, Record.class);
                 Record o = aaa.get();
-                //long endTime = System.nanoTime();
-                //System.out.print(".");
-                //   System.out.println("time:" + (endTime - startTime) / 1000000.0 + " data: " + o.getCity());
+            } catch (Exception ex) {
+                System.out.println("RPC call failed. ex=" + ex.getMessage());
             }
-            Thread.sleep(50);
+            s.addValue((System.nanoTime()-start)/1000000.0);
+            if (DOSLEEP) {
+                Thread.sleep(50);
+            }
         }
     }
 }
