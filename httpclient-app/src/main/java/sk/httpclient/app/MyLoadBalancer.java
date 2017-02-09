0af673d904b01546f610c1b9132cb6dbbe9c212f
Ľubomír Varga <lubomir.varga@nike.sk>
2017-02-09 14:35:40 +0100

    Make load balancer as fast as possible. No config is used, servers are defined in code.

--- httpclient-app/src/main/java/sk/httpclient/app/MyLoadBalancer.java
+++ httpclient-app/src/main/java/sk/httpclient/app/MyLoadBalancer.java
@@ -1,25 +1,29 @@
 package sk.httpclient.app;
 
-import com.netflix.loadbalancer.DynamicServerListLoadBalancer;
+import com.netflix.loadbalancer.ILoadBalancer;
 import com.netflix.loadbalancer.Server;
 
 import java.util.List;
 
-public class MyLoadBalancer extends DynamicServerListLoadBalancer {
-
+public class MyLoadBalancer implements ILoadBalancer {
     private int c = 0;
 
     @Override
 
     public void addServers(List<Server> newServers) {
-        super.addServers(newServers);
+        //super.addServers(newServers);
     }
+
+    private final Server[] a = new Server[]{
+        new Server("localhost", 8887),
+        new Server("localhost", 8888),
+        new Server("localhost", 8889)
+    };
 
     @Override
     public Server chooseServer(Object key) {
-        //c = ++c % upServerList.size();
-        //return upServerList.get(c);
-        return super.chooseServer(key);
+        c = ++c % 3;
+        return a[c];
     }
 
     @Override
