e7da965f447a838fd1bef0bc86b1a086d2367d80
Ľubomír Varga <lubomir.varga@nike.sk>
2017-02-09 14:50:14 +0100

    Some changes in sample RibbonHttpClient.
    Added main method with small test code.
    Changed some parameters.
    Make send method do minimum work to be fast (price it that it is sending always same request for now).
    Better handling of errors in deserialize phase.
    Added and commented out also other possible endpoints (those in /problems/ path.

--- httpclient-app/src/main/java/sk/httpclient/app/RibbonHttpClient.java
+++ httpclient-app/src/main/java/sk/httpclient/app/RibbonHttpClient.java
@@ -9,23 +9,34 @@ import com.netflix.ribbon.RibbonRequest;
 import com.netflix.ribbon.http.HttpRequestTemplate;
 import com.netflix.ribbon.http.HttpResourceGroup;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.reactivex.netty.protocol.http.client.HttpClientResponse;
 import rx.Observable;
 import rx.functions.Func1;
 
-import java.io.IOException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class RibbonHttpClient<R, T> implements MyHttpClient<R, T> {
 
-    private ObjectMapper mapper = new ObjectMapper();
-    private HttpRequestTemplate.Builder<ByteBuf> requestBuilder;
+    private final ObjectMapper mapperDefault = new ObjectMapper();
+    // private final ObjectMapper mapperProto = new ObjectMapper(new ProtobufFactory());
+    public HttpResourceGroup resourceGroup;
+    HttpRequestTemplate<ByteBuf> service;
+    RibbonRequest<ByteBuf> req;
 
     public RibbonHttpClient(String servers) {
-        HttpResourceGroup resourceGroup = Ribbon.createHttpResourceGroup("sample-client", config(servers));
-        requestBuilder = resourceGroup.newTemplateBuilder("sample-client");
+        resourceGroup = Ribbon.createHttpResourceGroup("sample-client", config(servers));
+        HttpRequestTemplate.Builder<ByteBuf> builder = resourceGroup.newTemplateBuilder("sample-client");
+        service = builder
+            .withMethod("GET")
+            .withUriTemplate("/test/record")
+            //.withUriTemplate("/problems/systemExit")
+            //.withUriTemplate("/problems/stackOverflow")
+            //.withUriTemplate("/problems/runtimeException")
+            //.withHystrixProperties(com.netflix.hystrix.HystrixObservableCommand.Setter.withGroupKey())
+            .build();
+        req = service.requestBuilder().build();
+
     }
 
     private Func1<HttpClientResponse<ByteBuf>, Observable<T>> getContent(Class<T> clazz) {
@@ -36,8 +47,14 @@ public class RibbonHttpClient<R, T> implements MyHttpClient<R, T> {
         try {
             byte[] bytes = new byte[buf.readableBytes()];
             buf.readBytes(bytes);
-            return mapper.readValue(bytes, clazz);
-        } catch (IOException e) {
+            T t;
+            try {
+                t = mapperDefault.readValue(bytes, clazz);
+            } catch (Exception ex) {
+                throw new RuntimeException("clazz=" + clazz + ";buf=" + new String(bytes) + "...;", ex);
+            }
+            return t;
+        } catch (RuntimeException e) {
             e.printStackTrace();
         }
         return null;
@@ -45,18 +62,18 @@ public class RibbonHttpClient<R, T> implements MyHttpClient<R, T> {
 
     private ClientOptions config(String servers) {
         IClientConfig clientConfig = IClientConfig.Builder.newBuilder("sample-client").build();
-        //clientConfig.set(CommonClientConfigKey.NFLoadBalancerClassName, "sk.httpclient.app.MyLoadBalancer");
+        clientConfig.set(CommonClientConfigKey.NFLoadBalancerClassName, "sk.httpclient.app.MyLoadBalancer");
         clientConfig.set(CommonClientConfigKey.InitializeNFLoadBalancer, true);
         clientConfig.set(CommonClientConfigKey.ListOfServers, servers);
-        clientConfig.set(CommonClientConfigKey.MaxAutoRetriesNextServer, 40);
-        clientConfig.set(CommonClientConfigKey.MaxAutoRetries, 40);
+        clientConfig.set(CommonClientConfigKey.MaxAutoRetriesNextServer, 400);
+        clientConfig.set(CommonClientConfigKey.MaxAutoRetries, 400);
         clientConfig.set(CommonClientConfigKey.EnableConnectionPool, true);
-        clientConfig.set(CommonClientConfigKey.PoolMaxThreads, 1500);
-        clientConfig.set(CommonClientConfigKey.PoolMinThreads, 300);
-        clientConfig.set(CommonClientConfigKey.PoolKeepAliveTime, 1000);
-        clientConfig.set(CommonClientConfigKey.PoolKeepAliveTimeUnits, TimeUnit.SECONDS.name()); //TODO ake su tu hodnoty?
-        clientConfig.set(CommonClientConfigKey.MaxConnectionsPerHost, 500);
-        clientConfig.set(CommonClientConfigKey.MaxTotalConnections, 300);
+        clientConfig.set(CommonClientConfigKey.PoolMaxThreads, 50);
+        clientConfig.set(CommonClientConfigKey.PoolMinThreads, 42);
+        clientConfig.set(CommonClientConfigKey.PoolKeepAliveTime, 100000000);
+        clientConfig.set(CommonClientConfigKey.PoolKeepAliveTimeUnits, TimeUnit.SECONDS.toString()); //TODO ake su tu hodnoty?
+        clientConfig.set(CommonClientConfigKey.MaxConnectionsPerHost, 20);
+        clientConfig.set(CommonClientConfigKey.MaxTotalConnections, 70);
         clientConfig.set(CommonClientConfigKey.EnablePrimeConnections, true);
         clientConfig.set(CommonClientConfigKey.EnablePrimeConnections, true);
         return ClientOptions.from(clientConfig);
@@ -65,19 +82,36 @@ public class RibbonHttpClient<R, T> implements MyHttpClient<R, T> {
 
     @Override
     public Future<T> send(String procedureName, R request, Class<T> clazz) {
-
-
-        ByteBuf buf = Unpooled.copiedBuffer("test content".getBytes());
-
-        HttpRequestTemplate<ByteBuf> service = requestBuilder
-                .withMethod("POST")
-                .withUriTemplate("/test")
-                .build();
-
-        RibbonRequest<ByteBuf> req = service.requestBuilder().withContent(Observable.just(buf)).build();
-
-        return req.toObservable().map(buff -> convert(clazz, buff)).toBlocking().toFuture();
-
+        return req.toObservable()
+            .map(buff -> convert(clazz, buff))
+            .doOnError(throwable -> {
+                throw new RuntimeException("Error while doing RPC!", throwable);
+            })
+            .toBlocking()
+            .toFuture();
     }
 
+    // minimal testing code with Tomas
+    public static void main(String[] args) {
+        HttpResourceGroup httpResourceGroup = Ribbon.createHttpResourceGroup("movieServiceClient",
+            ClientOptions.create()
+                .withMaxAutoRetriesNextServer(0)
+                .withConfigurationBasedServerList("localhost:8889,localhost:8888,localhost:8887"));
+        HttpRequestTemplate<ByteBuf> recommendationsByUserIdTemplate = httpResourceGroup.newTemplateBuilder("recommendationsByUserId", ByteBuf.class)
+            .withMethod("POST")
+            .withUriTemplate("/test/record")
+
+            .build();
+        for (int i = 0; i < 500; i++) {
+            try {
+                final String x = recommendationsByUserIdTemplate.requestBuilder()
+                    .build()
+                    .execute().toString();
+                System.out.println(x);
+            } catch (Exception e) {
+                System.out.println("nepreslo: " + e.getMessage());
+            }
+        }
+        // TODO hystrix spojit s codahhale metrics registry a cez console reporter vypisovat. Cielom je zistit, ktory circuit breaker sa otvoril...
+    }
 }
