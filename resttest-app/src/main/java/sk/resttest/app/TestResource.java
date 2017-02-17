8c37c204421a70a4014bcfc319e3d6ba9046f2f8
Ľubomír Varga <lubomir.varga@nike.sk>
2017-02-17 13:00:17 +0100

    Server side logging. Also configured logback.

--- resttest-app/src/main/java/sk/resttest/app/TestResource.java
+++ resttest-app/src/main/java/sk/resttest/app/TestResource.java
@@ -7,24 +7,21 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Path("/test")
 public class TestResource {
-    private final Logger LOG = LoggerFactory.getLogger(TestResource.class);
-    static AtomicInteger counter = new AtomicInteger(0);
-    static AtomicInteger failCode = new AtomicInteger(0);
+    private static final Logger LOG = LoggerFactory.getLogger(TestResource.class);
+    private static AtomicInteger recordPostCount = new AtomicInteger(0);
+    private static AtomicInteger failCode = new AtomicInteger(0);
     private static long sleepTime = 0;
     private static boolean throwException = false;
 
     @GET
     @Path("/record")
-    @Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
     @Produces(MediaType.APPLICATION_JSON)
     public String json() {
-        System.out.println("received: get");
-
+        LOG.debug("GET  on /test/record");
         currentRecordBehavior();
         return "{ \"name\":\"John\", \"age\":31, \"city\":\"Get New York\" }";
     }
@@ -34,9 +31,9 @@ public class TestResource {
     @Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
     @Produces(MediaType.APPLICATION_JSON)
     public String json(String data) {
-        System.out.println("received: " + data + " count: " + counter.incrementAndGet());
+        LOG.debug("POST on /test/record. recordPostCount={}, data={}.", recordPostCount.incrementAndGet(), data);
         currentRecordBehavior();
-        return "{ \"name\":\"John\", \"age\":31, \"city\":\"Post New York\" }";
+        return data;
     }
 
     @POST
@@ -77,8 +74,7 @@ public class TestResource {
     @Consumes(MediaType.APPLICATION_OCTET_STREAM)
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
     public String proto(InputStream is) {
-        System.out.println("received: octet");
-
+        LOG.debug("POST on /test/record. recordPostCount={}, data=inputstream.", recordPostCount.incrementAndGet());
         currentRecordBehavior();
         // TODO implement correct response https://dennis-xlc.gitbooks.io/restful-java-with-jax-rs-2-0-2rd-edition/content/en/part1/chapter6/custom_marshalling.html
         return "{ \"name\":\"John\", \"age\":31, \"city\":\"Post New York\" }";
@@ -89,15 +85,23 @@ public class TestResource {
     @Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
     @Produces(MediaType.APPLICATION_JSON)
     public String jsonCall(String data) {
-        System.out.println("received call: " + data);
+        LOG.debug("POST on /test/call. data={}.", data);
+        return data;
+    }
+
+    @GET
+    @Path("/call")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String jsonCall() {
+        LOG.debug("GET  on /test/call");
         return "{ \"name\":\"Cyril\", \"age\":22, \"city\":\"soap call\" }";
     }
 
     @GET
     @Path("/control")
-    @Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN, MediaType.APPLICATION_OCTET_STREAM})
     @Produces(MediaType.APPLICATION_JSON)
     public String controlGet(String data) {
+        LOG.debug("GET  on /test/control. data={}.", data);
         return this.control(data);
     }
 
@@ -106,21 +110,20 @@ public class TestResource {
     @Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN, MediaType.APPLICATION_OCTET_STREAM})
     @Produces(MediaType.APPLICATION_JSON)
     public String controlPost(String data) {
+        LOG.debug("POST on /test/control. data={}.", data);
         return this.control(data);
     }
 
     /**
-     *
-     * @param data  seems to come from client something like "\"1,12\"" for example.
+     * @param data seems to come from client something like "\"1,12\"" for example.
      * @return
      */
     public String control(String data) {
-        System.out.println("!!!!!!!!!!control(data=" + data + ")");
-        System.out.println("Control data " + data);
+        LOG.debug("\tcontrol /test/control. data={}.", data);
         if (data.startsWith("\"fail")) {
             Integer errorCode = Integer.valueOf(data.substring(1, data.length() - 1).split(" ")[1]);
-            System.out.println("failing with " + errorCode);
             failCode.set(errorCode);
+            LOG.info("\tcontroll have set to fail with errorCode={}.", errorCode);
             return "{}";
         }
         if (data.startsWith("\"4")) {
@@ -136,7 +139,7 @@ public class TestResource {
             long timeoutSleep = Long.parseLong(splitted[1].replace("\"", ""));
 
             this.sleepTime = timeoutSleep;
-            LOG.info("control is going to 3. Overload state. Sleep time in ms:" + timeoutSleep);
+            LOG.info("control is going to 3. Overload state. Sleep time in ms:{}.", timeoutSleep);
             return "{ \"name\":\"XXX\", \"age\":" + timeoutSleep + ", \"city\":\"TIMEOUT SIMULATION\" }";
         }
         if (data.startsWith("\"2")) {
@@ -145,10 +148,9 @@ public class TestResource {
             return "{ \"name\":\"XXX\", \"age\":31, \"city\":\"PROCESSING EXCEPTION SIMULATION\" }";
         }
         if (data.startsWith("\"1")) {
-            System.out.println("data.startsWith(\"1\"):" + data);
             String[] splitted = data.split(",");
             int deploySleep = Integer.parseInt(splitted[1].replace("\"", ""));
-            LOG.info("control is going to 1. Restart (simulated deploy) state. Restart seconds: " + deploySleep);
+            LOG.info("control is going to 1. Restart (simulated deploy) state. Restart seconds:{}.", deploySleep);
             new Thread(() -> {
                 try {
                     Thread.sleep(42);
@@ -167,7 +169,7 @@ public class TestResource {
      */
     private void currentRecordBehavior() {
         if (this.sleepTime != 0) {
-            System.out.println("this.sleepTime=" + this.sleepTime);
+            LOG.trace("this.sleepTime={}.", this.sleepTime);
             try {
                 Thread.sleep(this.sleepTime);
             } catch (InterruptedException e) {
@@ -175,7 +177,7 @@ public class TestResource {
             }
         }
         if (this.throwException == true) {
-            System.out.println("this.throwException=" + this.throwException);
+            LOG.trace("this.throwException={}.", this.throwException);
             throw new RuntimeException("Some bad exception during fulfilling request happened. This should be simulation of database request failed or so.");
         }
     }
